252d1aebe51510488ef476dd0164dffa016d0e72
Achim Herwig <achim.herwig@wodca.de>
2013-01-13 20:30:05 +0100

    First draft of the UI.

--- Papierlos.py
+++ Papierlos.py
@@ -7,10 +7,7 @@ class MainWindow(QtGui.QMainWindow):
         super(MainWindow, self).__init__()
         self.ui = mainwindow.Ui_MainWindow()
         self.ui.setupUi(self)
-        self.ui.pushButton.clicked.connect(self.advanceSlider)
-
-    def advanceSlider(self):
-        self.ui.progressBar.setValue(self.ui.progressBar.value() + 1)
+        self.ui.saveButton.clicked.connect(self.close)
 
 app = QtGui.QApplication(sys.argv)
 
