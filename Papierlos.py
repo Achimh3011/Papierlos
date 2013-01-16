78e361ef5783fcf2647bb01c36204bc4dbf2ea4a
Achim Herwig <achim.herwig@wodca.de>
2013-01-16 22:01:30 +0100

    Reorganised main window widget creation.

--- Papierlos.py
+++ Papierlos.py
@@ -2,16 +2,10 @@ import sys
 from PyQt4 import QtGui
 import mainwindow
 
-class MainWindow(QtGui.QMainWindow):
-    def __init__(self):
-        super(MainWindow, self).__init__()
-        self.ui = mainwindow.Ui_MainWindow()
-        self.ui.setupUi(self)
-        self.ui.saveButton.clicked.connect(self.close)
 
 app = QtGui.QApplication(sys.argv)
 
-my_mainWindow = MainWindow()
-my_mainWindow.show()
+mainWindow = mainwindow.MainWindow()
+mainWindow.show()
 
 sys.exit(app.exec_())
--- mainwindow.py
+++ mainwindow.py
@@ -22,92 +22,84 @@ except AttributeError:
     def _translate(context, text, disambig):
         return QtGui.QApplication.translate(context, text, disambig)
 
-class Ui_MainWindow(object):
-    def setupUi(self, MainWindow):
-        self.centralwidget = QtGui.QWidget(MainWindow)
+class MainWindow(QtGui.QMainWindow):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+        self.setupUi()
+        self.saveButton.clicked.connect(self.close)
+        self.actionQuit.activated.connect(self.close)
 
-        verticalLayout_2 = QtGui.QVBoxLayout(self.centralwidget)
-        verticalLayout_2.setMargin(0)
-        horizontalLayout = QtGui.QHBoxLayout()
+    def setupUi(self):
+        self.centralwidget = QtGui.QWidget(self)
 
         self.scanButton = KPushButton(self.centralwidget)
-        horizontalLayout.addWidget(self.scanButton)
-
         spacerItem = QtGui.QSpacerItem(40, 20, QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Minimum)
-        horizontalLayout.addItem(spacerItem)
-
         self.reduceColorsButton = KPushButton(self.centralwidget)
-        horizontalLayout.addWidget(self.reduceColorsButton)
-
         self.numColorSpinBox = KIntSpinBox(self.centralwidget)
         self.numColorSpinBox.setMinimum(2)
         self.numColorSpinBox.setMaximum(16)
         self.numColorSpinBox.setProperty("value", 8)
-        horizontalLayout.addWidget(self.numColorSpinBox)
-        verticalLayout_2.addLayout(horizontalLayout)
+
+        scanSettingsLayout = QtGui.QHBoxLayout()
+        scanSettingsLayout.addWidget(self.scanButton)
+        scanSettingsLayout.addItem(spacerItem)
+        scanSettingsLayout.addWidget(self.reduceColorsButton)
+        scanSettingsLayout.addWidget(self.numColorSpinBox)
 
         self.colorEditorCells = KColorCells(self.centralwidget,4,4)
-        verticalLayout_2.addWidget(self.colorEditorCells)
 
         self.previewView = QtGui.QGraphicsView(self.centralwidget)
         self.previewView.setMinimumSize(QtCore.QSize(400, 400))
-        verticalLayout_2.addWidget(self.previewView)
-
-        verticalLayout = QtGui.QVBoxLayout()
-        horizontalLayout_2 = QtGui.QHBoxLayout()
-        horizontalLayout_3 = QtGui.QHBoxLayout()
 
         self.baseFileNameRequester = KUrlRequester(self.centralwidget)
-        horizontalLayout_3.addWidget(self.baseFileNameRequester)
-
         self.FileSeqSpinBox = KIntSpinBox(self.centralwidget)
-        horizontalLayout_3.addWidget(self.FileSeqSpinBox)
 
-        horizontalLayout_2.addLayout(horizontalLayout_3)
-
-        verticalLayout.addLayout(horizontalLayout_2)
-
-        horizontalLayout_5 = QtGui.QHBoxLayout()
+        filenameLayout = QtGui.QHBoxLayout()
+        filenameLayout.addWidget(self.baseFileNameRequester)
+        filenameLayout.addWidget(self.FileSeqSpinBox)
 
         self.generatedOutputNameTextLabel = QtGui.QLabel(self.centralwidget)
-        horizontalLayout_5.addWidget(self.generatedOutputNameTextLabel)
-
         self.generatedOutputNameLabel = QtGui.QLabel(self.centralwidget)
-        horizontalLayout_5.addWidget(self.generatedOutputNameLabel)
-        verticalLayout.addLayout(horizontalLayout_5)
 
-        horizontalLayout_4 = QtGui.QHBoxLayout()
+        outputNameLayout = QtGui.QHBoxLayout()
+        outputNameLayout.addWidget(self.generatedOutputNameLabel)
+        outputNameLayout.addWidget(self.generatedOutputNameTextLabel)
+
         spacerItem1 = QtGui.QSpacerItem(40, 20, QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Minimum)
-        horizontalLayout_4.addItem(spacerItem1)
         self.saveButton = KPushButton(self.centralwidget)
-        horizontalLayout_4.addWidget(self.saveButton)
 
-        verticalLayout.addLayout(horizontalLayout_4)
+        saveButtonLayout = QtGui.QHBoxLayout()
+        saveButtonLayout.addItem(spacerItem1)
+        saveButtonLayout.addWidget(self.saveButton)
 
-        verticalLayout_2.addLayout(verticalLayout)
+        verticalLayout = QtGui.QVBoxLayout(self.centralwidget)
+        verticalLayout.setMargin(0)
+        verticalLayout.addLayout(scanSettingsLayout)
+        verticalLayout.addWidget(self.colorEditorCells)
+        verticalLayout.addWidget(self.previewView)
+        verticalLayout.addLayout(filenameLayout)
+        verticalLayout.addLayout(outputNameLayout)
+        verticalLayout.addLayout(saveButtonLayout)
 
-        MainWindow.setCentralWidget(self.centralwidget)
+        self.setCentralWidget(self.centralwidget)
 
-        self.menubar = QtGui.QMenuBar(MainWindow)
+        self.menubar = QtGui.QMenuBar(self)
         self.menubar.setGeometry(QtCore.QRect(0, 0, 474, 20))
         self.menuFile = QtGui.QMenu(self.menubar)
-        MainWindow.setMenuBar(self.menubar)
+        self.setMenuBar(self.menubar)
 
-        self.statusbar = QtGui.QStatusBar(MainWindow)
+        self.statusbar = QtGui.QStatusBar(self)
         self.statusbar.setMinimumSize(QtCore.QSize(0, 20))
-        MainWindow.setStatusBar(self.statusbar)
+        self.setStatusBar(self.statusbar)
 
-        self.actionQuit = QtGui.QAction(MainWindow)
+        self.actionQuit = QtGui.QAction(self)
         self.menuFile.addAction(self.actionQuit)
         self.menubar.addAction(self.menuFile.menuAction())
 
-        self.retranslateUi(MainWindow)
+        self.retranslateUi()
 
-        QtCore.QObject.connect(self.actionQuit, QtCore.SIGNAL(_fromUtf8("activated()")), MainWindow.close)
-        QtCore.QMetaObject.connectSlotsByName(MainWindow)
-
-    def retranslateUi(self, MainWindow):
-        MainWindow.setWindowTitle(kdecore.i18n(_fromUtf8("MainWindow")))
+    def retranslateUi(self):
+        self.setWindowTitle(kdecore.i18n(_fromUtf8("MainWindow")))
         self.scanButton.setText(kdecore.i18n(_fromUtf8("Scan")))
         self.reduceColorsButton.setText(kdecore.i18n(_fromUtf8("Reduce Colors")))
         self.generatedOutputNameTextLabel.setText(kdecore.i18n(_fromUtf8("Output:")))
@@ -115,6 +107,9 @@ class Ui_MainWindow(object):
         self.saveButton.setText(kdecore.i18n(_fromUtf8("Save")))
         self.menuFile.setTitle(kdecore.i18n(_fromUtf8("File")))
         self.actionQuit.setText(kdecore.i18n(_fromUtf8("Quit")))
+
+    def scan(self):
+        pass
 
 from PyKDE4.kio import KUrlRequester
 from PyKDE4.kdeui import KColorCells, KPushButton, KIntSpinBox
