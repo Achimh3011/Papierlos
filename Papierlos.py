11e543b5006081b7ed3d27b79705c338302f7991
Achim Herwig <achim.herwig@wodca.de>
2013-01-25 21:51:19 +0100

    Error message if no scanner is found.

--- Papierlos.py
+++ Papierlos.py
@@ -1,5 +1,6 @@
 import sys
 from PyQt4 import QtGui
+from PyKDE4 import kdeui
 import mainwindow
 import sane
 
@@ -7,6 +8,9 @@ app = QtGui.QApplication(sys.argv)
 
 sane.init()
 devices = sane.get_devices()
+if not devices:
+    kdeui.KMessageBox.error(None, 'No scanner device found.', 'No Scanner')
+    sys.exit(1)
 
 mainWindow = mainwindow.MainWindow()
 mainWindow.device = sane.open(devices[0][0])
